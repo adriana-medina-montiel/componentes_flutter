287c2a7559c009b073c8b1d8ab13a2b432f41ab3
adriana <adrimedinamontiel12@gmail.com>
2024-02-21 11:01:09 -0600

    navigator.push to inputScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/screens/inputs_screen.dart';
 import 'package:practica3/theme/app_theme.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -19,6 +20,12 @@ class HomeScreen extends StatelessWidget {
             subtitle: Text('introduce valores ', style: Apptheme.darkTheme.textTheme.bodySmall),
             leading: const Icon(Icons.power_input),
             trailing: const Icon(Icons.arrow_circle_down),
+            onTap: () {
+              final ruta1 = MaterialPageRoute(builder: (context){
+                return const InputScreen();
+              });
+              Navigator.push(context,ruta1);
+            },
           ),
           Divider(),
            ListTile (
