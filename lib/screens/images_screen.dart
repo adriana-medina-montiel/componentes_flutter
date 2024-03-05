a845263cd7282d94b6103d5c2965255aabc10f0d
adriana <adrimedinamontiel12@gmail.com>
2024-03-05 12:01:47 -0600

    images

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/theme/app_theme.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ImagesScreen extends StatefulWidget {
   const ImagesScreen({super.key});
@@ -13,13 +15,62 @@ class _ImageScreenState extends State<ImagesScreen> {
     return Scaffold( appBar: AppBar( 
       title: const Text('imagenes'),
       ),
-      body: const Column(
+      body: ListView(
         children: [
-          Image(
-            image: AssetImage('assets/img/hola.jpg')
-          )
+         cardImage1(),
+         ImageFade()
         ],
       )
       );
+  }
+  Card cardImage1(){
+    return Card(
+      shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+        margin: const EdgeInsets.all(16),
+        elevation: 10,
+    
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Column(
+          children: [ 
+          const SizedBox(
+            height: 350.0,
+            width: 350.0,
+            child: Image(
+              image: AssetImage('assets/img/hola.jpg')
+            ),
+          ),
+          Container(
+              padding:const EdgeInsets.all(10),
+              child: Text(
+                'Un gato saludando',
+                style: Apptheme.darkTheme.textTheme.headlineMedium,
+              ),
+            )
+       ]
+          ),
+        ),
+      );
+  }
+  SizedBox image2(){
+    return SizedBox(
+      height: 200.0,
+      width: 100.0,
+      child: Image.network('https://png.pngtree.com/png-clipart/20190330/ourlarge/pngtree-cartoon-cow-black-and-white-hand-painted-elements-png-image_893878.jpg'),
+    );
+
+  }
+  Stack ImageFade(){
+    return Stack( 
+      children: <Widget> [
+        Center(
+          child: CircularProgressIndicator(),
+        ),
+        Center(child: FadeInImage.memoryNetwork(
+           placeholder: kTransparentImage,
+           image: 'https://png.pngtree.com/png-clipart/20190330/ourlarge/pngtree-cartoon-cow-black-and-white-hand-painted-elements-png-image_893878.jpg'),),
+      ],
+    );
   }
 }
